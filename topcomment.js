40d77437461d32bb0f3d9cb94f2c05aa21d0c310
Hamish Macpherson <hamstu@gmail.com>
2013-01-21 11:11:34 -0800

    Removed unnecessary console.log() code.

--- topcomment.js
+++ topcomment.js
@@ -12,7 +12,6 @@ TopComment =
 
   init : function()
   {
-    console.log("TopComment.init();");
     $("a.comments").each(TopComment.hookEachComment);
   },
 
@@ -36,28 +35,24 @@ TopComment =
         TopComment.cancelXHR();
 
         json_url = $(el).attr('href') + '.json';
-        console.log("URL: " + json_url);
         top_comment = false;
 
         TopComment.showCommentLoading(el);
 
         if (json_url in TopComment.comment_cache)
         {
-          console.log("Found comment in cache!");
           top_comment = TopComment.comment_cache[json_url];
           TopComment.showTopComment(el, top_comment);
         }
         else
         {
-          console.log("Fetching comments from JSON!");
           _xhr = $.getJSON(json_url, function(data)
           {
-            console.log("Got comment data! Extracting top_comment");
             try {
               top_comment = data[1]['data']['children'][0]['data'];
               TopComment.showTopComment(el, top_comment);
             } catch(error) {
-              console.log("Error extracting comment: " + error.message);
+              console.error("Error extracting comment: " + error.message);
             }
             TopComment.comment_cache[json_url] = top_comment;
           });
@@ -95,13 +90,11 @@ TopComment =
   },
   showCommentLoading : function (el)
   {
-    console.log("showCommentLoading");
     $(TopComment.getMessageDIV(el)).html("<span class='md'>Loading top comment...</span>");
   },
   showTopComment : function (el, top_comment)
   {
     // Text
-    console.log("showTopComment: " + top_comment['body_html']);
     var body_html = $("<div/>").html(top_comment['body_html']).text(); // quick method to decode entities
     var tagline = $("<p/>").addClass('tagline');
       var author = $("<a/>").html(top_comment['author']).attr('href', 'http://reddit.com/user/' + top_comment['author']).addClass('author');
